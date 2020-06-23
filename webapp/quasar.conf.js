096d7667037f62913af8ad164d538eff342fe7e9
luozhongdao <v768851314>
2020-06-23 23:03:53 +0800

    update

--- webapp/quasar.conf.js
+++ webapp/quasar.conf.js
@@ -129,9 +129,9 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
-        description: 'A Quasar Framework app',
+        name: 'glance',
+        short_name: 'glance',
+        description: 'glance',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
